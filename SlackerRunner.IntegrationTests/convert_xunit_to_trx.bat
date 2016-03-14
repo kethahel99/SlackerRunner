a0a4f5839718a55cac310430901a5eddcc52c7f0
ornatwork <ornsmail@gmail.com>
2016-03-14 10:45:28 -0600

    updated to use the xunit to trx transform

--- SlackerRunner.IntegrationTests/convert_xunit_to_trx.bat
+++ SlackerRunner.IntegrationTests/convert_xunit_to_trx.bat
@@ -1,1 +1,1 @@
-build_tools\nxslt\nxslt3.exe xunit_results.xml build_tools\nxslt\NUnitToMSTest_solvas.xslt -o vstest_results.trx
+build_tools\nxslt\nxslt3.exe xunit_results.xml build_tools\nxslt\Xunit_to_MsTest.xslt -o vstest_results.trx
