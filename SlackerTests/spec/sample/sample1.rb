aba128892785bbc83565b3a52e34f32a5a9eab58
Shkolnik, John <jshkolnik@deloitte.com>
2016-07-18 19:19:44 -0400

    Accidentally modified sample1.rb.

--- SlackerTests/spec/sample/sample1.rb
+++ SlackerTests/spec/sample/sample1.rb
@@ -4,7 +4,7 @@ describe 'My database' do
   # Simple inline query example.
   it 'contains system tables' do
     # Make sure we have at least one system object in the database.
-    expect( query("select * from sysobjects where xtype = 'S';").count ).to be < 0
+    expect( query("select * from sysobjects where xtype = 'S';").count ).to be > 0
   end
 
     # Simple inline query example.
