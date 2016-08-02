1981e78e2127e1d2243487f993091d5c05a15487
orn <ornsmail@gmail.com>
2016-08-01 18:01:48 -0600

    Missing tests files added, included in project as well

---
+++ SlackerRunner.IntegrationTests/SpecificSpecTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using SlackerRunner;
+
+
+namespace SlackerRunner.IntegrationTests
+{
+  public class SpecificSpecTester
+  {
+
+    /// <summary>
+    /// Runs the Spec test in the file specified 
+    /// </summary>
+    /// <param name="rbFile"></param>
+    [Fact]
+    public void RunThisSpec()
+    {
+      Assert.True(TestFile(@"sample\sample1.rb"), "Test should have succeeded.");
+    }
+
+
+    // runs the passed file and reports on the result
+    private bool TestFile(string specFile)
+    {
+      SlackerResults SlackerResults = new SlackerService().Run(SpecsTester.RUN_TEST_DIR, SpecsTester.SPEC_TEST_DIR + specFile );
+      return SlackerResults.Passed;
+    }
+
+  }
+}
