09884e7d0d29a632598fa09896c72888b7e491a8
orn <ornsmail@gmail.com>
2016-08-19 18:51:25 -0600

    Added SlackerException smoke test, took out ProfileRunner output file references

---
+++ SlackerRunner.IntegrationTests/SlackerExceptionTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using System.ComponentModel;
+using SlackerRunner.IntegrationTests.Util;
+
+
+namespace SlackerRunner.IntegrationTests
+{
+  class SlackerExceptionTest
+  {
+
+    [Fact]
+    public async void SmokeTest_SlackerException()
+    {
+      // Smoke testing SlackerException
+      Exception ex = await Record.ExceptionAsync(() =>
+      {
+        throw new SlackerException();
+      });
+
+      // Proof 
+      // Check the Exception thrown 
+      Assert.NotNull(ex);
+      Assert.True(ex is SlackerException);
+    }
+
+    [Fact]
+    public async void SmokeTest_SlackerExceptionWithMessage()
+    {
+      // Smoke testing SlackerException
+      string msg = "My message";
+      Exception ex = await Record.ExceptionAsync(() =>
+      {
+        throw new SlackerException(msg);
+      });
+
+      // Proof 
+      // Check the Exception thrown 
+      Assert.NotNull(ex);
+      Assert.True(ex is SlackerException);
+      Assert.True(ex.Message.Equals(msg));
+    }
+  }
+}
--- SlackerRunner/ProfileRunner.cs
+++ SlackerRunner/ProfileRunner.cs
@@ -5,51 +5,24 @@ namespace SlackerRunner
 {
     public class ProfileRunner 
     {
-        private readonly Action<string, string> createOutputFile;
-        private readonly Action<string, string, string, string> logResults;
         private readonly ProcessRunner processRunner;
-        private readonly Func<string, string, string> readOutputFile;
         private readonly ResultsParser resultsParser;
         
-  
-        public ProfileRunner(ProcessRunner processRunner, ResultsParser resultsParser, Action<string, string> createOutputFile, 
-                            Func<string, string, string> readOutputFile, Action<string, string, string, string> logResults)
+        public ProfileRunner(ProcessRunner processRunner, ResultsParser resultsParser )
         {
             this.processRunner = processRunner;
             this.resultsParser = resultsParser;
-            this.createOutputFile = createOutputFile;
-            this.readOutputFile = readOutputFile;
-            this.logResults = logResults;
         }
 
         public ProfileRunner()
-            : this(new ProcessRunner(), new ResultsParser(), CreateOutputFile, ReadOutputFile, LogResults) { }
-
+            : this(new ProcessRunner(), new ResultsParser() ) { }
+    
+    
         public SlackerResults Run(string testDirectory, string testFile)
         {
           processRunner.Run(testDirectory, testFile);
           return resultsParser.Parse(processRunner.StandardOutput, processRunner.StandardError);
         }
-     
-
-        private static void CreateOutputFile(string testDirectory, string outputFileName)
-        {
-            //using (File.Create(OutputFileFullName(testDirectory, outputFileName))) { }
-        }
-        private static void LogResults(string testDirectory, string outputFileName, string standardError, string standardOutput)
-        {
-          Logger.Log("~~~ Standard Error: " + standardError);
-          Logger.Log("~~~~ Standard Output: " + standardOutput);
-        }
-        private static string OutputFileFullName(string testDirectory, string outputFileName)
-        {
-            return "";
-        }
-        private static string ReadOutputFile(string testDirectory, string outputFileName)
-        {
-          return "";
-        }
-
-
+    
     }
 }
