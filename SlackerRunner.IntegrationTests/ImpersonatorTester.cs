af51e5f717a795994e4517646d7d80bf61e73974
orn <ornsmail@gmail.com>
2016-09-20 15:36:10 -0600

    Added ImpersonatorTest

---
+++ SlackerRunner.IntegrationTests/ImpersonatorTester.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using SlackerRunner;
+
+
+namespace SlackerRunner.IntegrationTests
+{
+  public class ImpersonatorTest
+  {
+
+    [Fact]
+    public void SmokeImpersonator()
+    {
+      Exception ex = Record.Exception(() =>
+      {
+        using (Impersonator pers = new Impersonator("invalid_user", "invalid_domain", "invalid_password"))
+        {
+          // Invalid user, null  
+          Assert.Null(pers);
+        }
+      });
+
+    }
+
+
+  }  // EOF
+}
+
