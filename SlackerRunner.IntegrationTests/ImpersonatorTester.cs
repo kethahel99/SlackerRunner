ec477e7ad1191f09f6c271240e9df148ba90b557
Orn Kristjansson <okristjansson@deloitte.com>
2016-10-10 17:31:36 -0600

    Set release version to correct non buggy release number, ImpersonatorTest fixed

--- SlackerRunner.IntegrationTests/ImpersonatorTester.cs
+++ SlackerRunner.IntegrationTests/ImpersonatorTester.cs
@@ -9,16 +9,18 @@ namespace SlackerRunner.IntegrationTests
   {
 
     [Fact]
-    public void SmokeImpersonatorWithInfo()
+    public void SmokeImpersonatorInvalidCreds()
     {
       Exception ex = Record.Exception(() =>
       {
         using (Impersonator pers = new Impersonator("invalid_user", "invalid_domain", "invalid_password"))
         {
-          // Invalid user, null  
-          Assert.Null(pers);
+          // Invalid user, this doesn't hit 
         }
       });
+      
+      // Exception was thrown
+      Assert.NotNull(ex);
     }
     
   }  // EOF
