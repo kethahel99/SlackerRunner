93df528faf4a570557d6832fd322c14eeb8b2783
orn <okristjansson@deloitte.com>
2017-08-09 17:38:24 -0600

    Add tests for wildcard scenarios

--- SlackerRunner.IntegrationTests/SpecificSpecTester.cs
+++ SlackerRunner.IntegrationTests/SpecificSpecTester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Xunit;
 using SlackerRunner;
 
@@ -17,6 +18,29 @@ namespace SlackerRunner.IntegrationTests
       SlackerResults SlackerResults = new SlackerService().Run(SpecsTester.RUN_TEST_DIR, SpecsTester.SPEC_TEST_DIR + @"sample\sample1.rb");
       Assert.True(SlackerResults.Passed);
     }
+
+    /// <summary>
+    /// Runs the slacker tests that match the criteria *.rb
+    /// </summary>
+    [Fact(Skip = "Live database needed")]
+    //[Fact]
+    public void RunSpecs()
+    {
+      SlackerResults SlackerResults = new SlackerService().Run(SpecsTester.RUN_TEST_DIR, SpecsTester.SPEC_TEST_DIR + @"sample\*.rb");
+      Assert.True( SlackerResults.PassedSpecs > 1, SlackerResults.Message );
+    }
+
+    /// <summary>
+    /// Runs the slacker tests that match the criteria 
+    /// </summary>
+    [Fact(Skip = "Live database needed, Slacker ** functionality needs to be fixed")]
+    //[Fact]
+    public void RunSpecsInDir()
+    {
+      SlackerResults SlackerResults = new SlackerService().Run(SpecsTester.RUN_TEST_DIR, SpecsTester.SPEC_TEST_DIR + @"sample\sampledir1\**");
+      Assert.True(SlackerResults.PassedSpecs > 1, SlackerResults.Message);
+    }
+
 
   }
 }
