87dc140788e3374878f8d2ace3d714d3ac74b336
orn <okristjansson@deloitte.com>
2017-08-10 09:16:10 -0600

    Add timeout test for RunDirectory

--- SlackerRunner.IntegrationTests/SpecsDirectoryTester.cs
+++ SlackerRunner.IntegrationTests/SpecsDirectoryTester.cs
@@ -13,20 +13,34 @@ namespace SlackerRunner.IntegrationTests
   {
     // Relative path to test dir 
     public static string RUN_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests")) + "\\";
-    public static string LONG_SPEC_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests", "spec", "sam ple") + "/");
+    public static string SPEC_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests", "spec") + "/");
 
 
     [Fact(Skip = "Live database needed")]
     //[Fact]
     public void runAllSpecsInDirectory()
     {
-      // Use explicit timeout as there are a few tests run at the same time ( ** )
-      int timeoutMilliseconds = 300 * 1000;
+      // Use explicit timeout as it's running all the tests in the spec directory
+      int timeoutMilliseconds = 100 * 1000;
       // Run all the tests in the directory at once 
-      SlackerResults SlackerResults = new SlackerService().RunDirectory(RUN_TEST_DIR, LONG_SPEC_TEST_DIR, timeoutMilliseconds );
-      // Proof it 
-      Assert.True(SlackerResults.PassedSpecs > 0);
-      Assert.True(SlackerResults.Passed, SlackerResults.Message);
+      SlackerResults SlackerResults = new SlackerService().RunDirectory(RUN_TEST_DIR, SPEC_TEST_DIR, timeoutMilliseconds );
+      // Proof it - got a few passed tests ( see debug for Slacker output )
+      Assert.True(SlackerResults.PassedSpecs > 7, SlackerResults.Message );
+    }
+
+    [Fact]
+    public void timeout()
+    {
+      // Test timeout by setting very small timeout threshold
+      int timeoutMilliseconds = 1;
+      Exception ex = Record.Exception(() =>
+      {
+        SlackerResults SlackerResults = new SlackerService().RunDirectory(RUN_TEST_DIR, SPEC_TEST_DIR, timeoutMilliseconds);
+      });
+
+      // Exception was thrown
+      Assert.NotNull(ex);
+      Assert.True(ex.ToString().IndexOf("timeout") > -1 );
     }
 
 
