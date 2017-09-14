a10e5db5ddb9b565dd8389cc004b70d68e166b86
orn <okristjansson@deloitte.com>
2017-09-14 15:39:39 -0600

    Add POCOs to extract Json data

---
+++ SlackerRunner/Poco/Example.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackerRunner.Poco
+{
+  public class Example
+  {
+    public string id { get; set; }
+    public string description { get; set; }
+    public string full_description { get; set; }
+    public string status { get; set; }
+    public string file_path { get; set; }
+    public int line_number { get; set; }
+    public float run_time { get; set; }
+    public string pending_message { get; set; }
+    public Exception exception { get; set; }
+  }
+}
---
+++ SlackerRunner/Poco/Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackerRunner.Poco
+{
+  public class Exception
+  {
+    public string Class { get; set; }
+    public string message { get; set; }
+    public List<string> backtrace { get; set; }
+
+  }
+}
