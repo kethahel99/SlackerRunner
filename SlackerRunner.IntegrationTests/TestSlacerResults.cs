5df89bb80368cec278516f301a494ef7e9b7a22c
orn <okristjansson@deloitte.com>
2017-09-15 16:48:30 -0600

    Add smoke test for SlackerResults

---
+++ SlackerRunner.IntegrationTests/TestSlacerResults.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//
+using Xunit;
+
+
+
+namespace SlackerRunner.IntegrationTests
+{
+  public class TestSlacerResults
+  {
+    
+    /// <summary>
+    /// Coverage test, these haven't been touched by other test code
+    /// </summary>
+    [Fact]
+    public void smokeTest()
+    {
+      SlackerResults res = new SlackerResults();
+
+      // excersize some props
+      res.File = "file";
+
+      // Proof it 
+      Assert.NotEmpty(res.File);
+      Assert.NotEmpty(res.ToString());
+    }
+
+  }
+}
+
