151892fb418148a8ef428ac31794c09278c0ad65
orn <okristjansson@deloitte.com>
2017-09-22 11:31:41 -0600

    Use DisplayName to override default test naming convention as discussed in issue #5

--- SlackerRunner.IntegrationTests/SpecsTester.cs
+++ SlackerRunner.IntegrationTests/SpecsTester.cs
@@ -13,7 +13,7 @@ namespace SlackerRunner.IntegrationTests
   {
     // Relative path to test dir 
     public static string RUN_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests")) + "\\";
-    public static string SPEC_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests", "spec" ) + "/");
+    public static string SPEC_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests", "spec") + "/");
     public static string LONG_SPEC_TEST_DIR = Path.GetFullPath(Path.Combine("..", "..", "..", "SlackerTests", "spec", "sam ple") + "/");
 
 
@@ -32,8 +32,8 @@ namespace SlackerRunner.IntegrationTests
     /// <summary>
     /// Runs Slacker spec tests 
     /// </summary>
-    [Theory(Skip = "Live database needed"), MemberData("TestFiles", typeof(IndividualSpecTestFile))]
-    //[Theory, MemberData("TestFiles", typeof(IndividualSpecTestFile))]
+    [Theory(DisplayName = "runSpecsIndividually", Skip = "Live database needed"), MemberData("TestFiles", typeof(IndividualSpecTestFile))]
+    //[Theory(DisplayName = "runSpecsIndividually"), MemberData("TestFiles", typeof(IndividualSpecTestFile))]
     public void runSpecsIndividually(ISpecTestFile File)
     {
       SlackerResults SlackerResults = new SlackerService().Run(RUN_TEST_DIR, LONG_SPEC_TEST_DIR + File.FileName);
@@ -43,11 +43,11 @@ namespace SlackerRunner.IntegrationTests
     /// <summary>
     /// Uses the SpecTesterResolver to figure out all the test files in a directory
     /// </summary>
-    public static IEnumerable<object[]> TestFiles( Type type )
+    public static IEnumerable<object[]> TestFiles(Type type)
     {
       // Pass either SpecTestFile to run tests in a group or IndividualTestFile to run one test file at a time 
       List<ISpecTestFile> files = Util.SpecsTesterResolver.ProcessDirectory(LONG_SPEC_TEST_DIR, type);
-      
+
       // Back to caller
       foreach (ISpecTestFile file in files)
         yield return new object[] { file };
