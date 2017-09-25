b06fa62f729485adfad9d9509d72aff71f506077
orn <okristjansson@deloitte.com>
2017-09-25 10:32:21 -0600

    Move build file

---
+++ build/update_version.ps1
@@ -0,0 +1,11 @@
+﻿<# 
+  Replaces version information in assembly 
+  Pass the version as parameter to this script, otherwize it will default to 0.0.0.7
+#>
+param (  [string]$version = "0.0.0.7"  )
+$x = 'Version("{0}")' -f $version
+$tmp = "Updating assembly Version: " 
+$tmp += $version
+write-output $tmp
+$content = Get-Content .\SlackerRunner\Properties\AssemblyInfo.cs
+$content -replace 'Version\(".*"\)',$x > .\SlackerRunner\Properties\AssemblyInfo.cs
