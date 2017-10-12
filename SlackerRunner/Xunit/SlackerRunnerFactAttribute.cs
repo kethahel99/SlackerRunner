907a8b294a422af21607c9a1aafeb83da9c73abd
orn <okristjansson@deloitte.com>
2017-10-12 16:03:08 -0600

    Remove xunit and ms runner dependencies that are not needed, caused side affects, issue #16

--- SlackerRunner/Xunit/SlackerRunnerFactAttribute.cs
+++ SlackerRunner/Xunit/SlackerRunnerFactAttribute.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using Xunit;
+using Xunit.Sdk;
 
 namespace SlackerRunner.Xunit
 {
+
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+  [XunitTestCaseDiscoverer("SlackerRunner.Xunit.XunitExtensions.SlackerFactDiscoverer", "SlackerRunner")]
   public sealed class SlackerRunnerFactAttribute : FactAttribute
   {
+    
     public SlackerRunnerFactAttribute([CallerMemberName] string memberName = null)
     {
       DisplayName = memberName;
     }
+    
   }
 
 }
--- SlackerRunner/Xunit/SlackerRunnerTheoryAttribute.cs
+++ SlackerRunner/Xunit/SlackerRunnerTheoryAttribute.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using Xunit;
+using Xunit.Sdk;
 
 namespace SlackerRunner.Xunit
 {
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+  [XunitTestCaseDiscoverer("SlackerRunner.Xunit.XunitExtensions.SlackerFactDiscoverer", "SlackerRunner")]
   public sealed class SlackerRunnerTheoryAttribute : TheoryAttribute
   {
     public SlackerRunnerTheoryAttribute([CallerMemberName] string memberName = null)
